d2d687966a35d49ca0a15a4ade7957aa1f0dc439
Zoltan Lederer <zoltanlederer@yahoo.com>
2021-12-01 19:17:07 +0100

    Blog list Exercises 4.9

--- part_4/bloglist-backend/tests/blog_api.test.js
+++ part_4/bloglist-backend/tests/blog_api.test.js
@@ -11,6 +11,14 @@ test('blog lists are returned as json', async () => {
     .expect('Content-Type', /application\/json/)
 }, 100000)
 
+test('id is an identifier', async () => {
+    const response = await api.get('/api/blogs')
+
+    const contents = response.body.map(r => r.id)
+    // const firstId = contents[0]
+    expect(contents[0]).toBeDefined()
+})
+
 afterAll(() => {
   mongoose.connection.close()
 })
