2498697fdfb4508f4b6f66408be05ed48c5c7c45
Zoltan Lederer <zoltanlederer@yahoo.com>
2021-12-07 22:00:17 +0100

    Blog list Exercises 4.18 - 4.20

--- part_4/bloglist-backend/controllers/blogs.js
+++ part_4/bloglist-backend/controllers/blogs.js
@@ -1,6 +1,15 @@
+const jwt = require('jsonwebtoken')
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+
+// const getTokenFrom = request => {
+//   const authorization = request.get('authorization')
+//   if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+//     return authorization.substring(7)
+//   }
+//   return null
+// }
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
@@ -15,7 +24,15 @@ blogsRouter.get('/', async (request, response) => {
 blogsRouter.post('/', async (request, response, next) => {
   const body = request.body
 
-  const user = await User.findById(body.userId)
+  const token = request.token
+  console.log('TOKEN', token)
+  const decodedToken = jwt.verify(token, process.env.SECRET)
+  if (!token || !decodedToken.id) {
+    return response.status(401).json({
+      error: 'token missing or invalid'
+    })
+  }
+  const user = await User.findById(decodedToken.id)
 
   const blog = new Blog ({
       title: body.title,
--- part_4/bloglist-backend/utils/middleware.js
+++ part_4/bloglist-backend/utils/middleware.js
@@ -19,13 +19,24 @@ const errorHandler = (error, request, response, next) => {
     return response.status(400).send({ error: 'malformatted id' })
   } else if (error.name === 'ValidationError') {
     return response.status(400).json({ error: error.message })
-  } 
+  } else if (error.name === 'JsonWebTokenError') {
+    return res.status(401).json({ error: error.message })
+}
 
   next(error)
+}
+
+const tokenExtractor = (req, res, next) => {
+  const authorization = req.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer')) {
+      req.token = authorization.substring(7)
+  }
+  next()
 }
 
 module.exports = {
   requestLogger,
   unknownEndpoint,
-  errorHandler
+  errorHandler,
+  tokenExtractor
 }
