f87cb3a270e735033939734cb388150a74daf56e
Zoltan Lederer <zoltanlederer@yahoo.com>
2021-12-13 13:34:12 +0100

    Bloglist Exercises 5.7

--- part_5/bloglist-frontend/src/components/Blog.js
+++ part_5/bloglist-frontend/src/components/Blog.js
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} - {blog.author} - {blog.url}
-  </div>  
-)
+const Blog = ({ blog }) => {
+  const [hiddenDetails, setHiddenDetails] = useState(true)
+
+  const detailVisible = { display: hiddenDetails ? 'none' : '' }
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title}&nbsp;
+        <button 
+          onClick={() => setHiddenDetails(!hiddenDetails)}>
+            {hiddenDetails ? 'View' : 'Hide'}
+        </button>
+        <div style={detailVisible}>
+          {blog.url} <br />
+          Likes: {blog.likes} <button>Like</button><br />
+          {blog.author}
+        </div>
+      </div>
+    </div> 
+  )   
+}
 
 export default Blog
