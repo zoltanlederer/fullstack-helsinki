93d8da597f6cbbf8239d8151e992a0bcd1b6f29d
Zoltan Lederer <zoltanlederer@yahoo.com>
2021-12-15 20:27:50 +0100

    Bloglist Exercises 5.14

--- part_5/bloglist-frontend/src/components/Blog.test.js
+++ part_5/bloglist-frontend/src/components/Blog.test.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
-import { render } from '@testing-library/react'
+import { render, fireEvent } from '@testing-library/react'
 import Blog from './Blog'
 
 
@@ -11,21 +11,35 @@ describe('Blog component test', () => {
     author: 'Bruce Wayne',
     url: 'https://www.batman.com',
     likes: 62,
+    user: {
+      username: 'root'
+    }
+  }
+
+  const currentUser = {
+    username: 'root'
   }
 
   let component
 
   beforeEach(() => {
     component = render(
-      <Blog blog={blog} />
+      <Blog blog={blog} currentUser={currentUser} />
     )
   })
 
-  test('renders content', () => {
+  test('component displaying a blog renders the blog\'s title and author, but does not render its url or number of likes by default', () => {
     expect(component.container).toHaveTextContent('Batman')
     expect(component.container).toHaveTextContent('Bruce Wayne')
     expect(component.container).not.toHaveTextContent('https://www.batman.com')
     expect(component.container).not.toHaveTextContent('62')
+  })
+
+  test('checks that the blog\'s url and number of likes are shown when the button controlling the shown details has been clicked', () => {
+    const showBtn = component.getByText('View')
+    fireEvent.click(showBtn)
+    expect(component.container).toHaveTextContent('https://www.batman.com')
+    expect(component.container).toHaveTextContent('62')
   })
 
 })
