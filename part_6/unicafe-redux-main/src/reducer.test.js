16d45b1982f8af456c2bf25876efe6aafa2531d9
Zoltan Lederer <zoltanlederer@yahoo.com>
2021-12-21 21:06:45 +0100

    Unicafe-redux Exercises 6.1

--- part_6/unicafe-redux-main/src/reducer.test.js
+++ part_6/unicafe-redux-main/src/reducer.test.js
@@ -13,7 +13,8 @@ describe('unicafe reducer', () => {
     const action = {
       type: 'DO_NOTHING'
     }
-
+    
+    deepFreeze(state)
     const newState = counterReducer(undefined, action)
     expect(newState).toEqual(initialState)
   })
@@ -32,4 +33,50 @@ describe('unicafe reducer', () => {
       bad: 0
     })
   })
+
+  test('ok is incremented', () => {
+    const action = {
+      type: 'OK'
+    }
+    const state = initialState
+
+    deepFreeze(state)
+    const newState = counterReducer(state, action)
+    expect(newState).toEqual({
+      good: 0,
+      ok: 1,
+      bad: 0
+    })
+  })
+
+  test('bad is incremented', () => {
+    const action = {
+      type: 'BAD'
+    }
+    const state = initialState
+
+    deepFreeze(state)
+    const newState = counterReducer(state, action)
+    expect(newState).toEqual({
+      good: 0,
+      ok: 0,
+      bad: 1
+    })
+  })
+
+  test('reset stats', () => {
+    const action = {
+      type: 'ZERO'
+    }
+    const state = initialState
+
+    deepFreeze(state)
+    const newState = counterReducer(state, action)
+    expect(newState).toEqual({
+      good: 0,
+      ok: 0,
+      bad: 0
+    })
+  })
+
 })
