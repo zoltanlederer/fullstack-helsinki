391d4389579a46837e427575fee89ec0d13c4f0a
Zoltan Lederer <zoltanlederer@yahoo.com>
2021-12-30 20:23:39 +0100

    Anecdotes Exercises 6.15

--- part_6/redux-anecdotes-main/src/reducers/anecdoteReducer.js
+++ part_6/redux-anecdotes-main/src/reducers/anecdoteReducer.js
@@ -1,3 +1,5 @@
+import anecdoteService from '../services/anecdotes'
+
 const reducer = (state = [], action) => {
   switch(action.type) {
     case 'INIT_ANECDOTES':
@@ -16,10 +18,13 @@ const reducer = (state = [], action) => {
   }
 }
 
-export const initializeAnecdotes = anecdotes => {
-  return {
-    type: 'INIT_ANECDOTES',
-    data: anecdotes,
+export const initializeAnecdotes = () => {
+  return async dispatch => {
+    const anecdotes = await anecdoteService.getAll()
+    dispatch({
+      type: 'INIT_ANECDOTES',
+      data: anecdotes,
+    })
   }
 }
 
