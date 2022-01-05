1b483e99b96f91eb2526ac73482f95166e658ffe
Zoltan Lederer <zoltanlederer@yahoo.com>
2022-01-05 15:56:39 +0100

    Routed Anecdotes Exercises 7.6

--- part_7/routed-anecdotes/src/App.js
+++ part_7/routed-anecdotes/src/App.js
@@ -71,9 +71,9 @@ const Footer = () => (
 )
 
 const CreateNew = (props) => {
-  const content = useField('text')
-  const author = useField('text')
-  const info = useField('text')
+  const { onReset: resetContent, ...content } = useField('text')
+  const { onReset: resetAuthor, ...author } = useField('text')
+  const { onReset: resetInfo, ...info } = useField('text')
 
   const handleSubmit = (e) => {
     e.preventDefault()
@@ -87,9 +87,9 @@ const CreateNew = (props) => {
 
   const handleReset = (e) => {
     e.preventDefault()
-    content.onReset()
-    author.onReset()
-    info.onReset()
+    resetContent()
+    resetAuthor()
+    resetInfo()
   }
 
   return (
